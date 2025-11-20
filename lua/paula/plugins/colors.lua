abf211bfbe4749f2f01031a3226326b8643a4c47
Paula Amaya <dev@paulaamaya.com>
2025-11-20 16:44:20 -0500

    Final color changes

--- lua/paula/plugins/colors.lua
+++ lua/paula/plugins/colors.lua
@@ -8,8 +8,8 @@ return {
       fade_nc = true,
       -- Customize the background color
       custom_highlights = {
-        Normal = { bg = "#1e222a" }, 
-        WinSeparator = { fg = "#81a1c1", bold = true }, 
+        Normal = { bg = "#1E222A" }, 
+        WinSeparator = { fg = "#81A1C1", bold = true }, 
       },
     })
 
