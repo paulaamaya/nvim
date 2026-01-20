3cad2d1d4f34e917c4d063c03ab424c795d4f6a0
Paula Amaya <dev@paulaamaya.com>
2026-01-20 13:26:15 -0500

    Potential fix for using minted Tex package on linux

--- lua/paula/plugins/vimtex.lua
+++ lua/paula/plugins/vimtex.lua
@@ -4,5 +4,14 @@ return {
   config = function()
     vim.g.vimtex_view_method = "skim" 
     vim.g.vimtex_quickfix_mode = 0
+    vim.g.vimtex_compiler_latexmk = {
+      options = {
+        '-verbose',
+        '-file-line-error',
+        '-synctex=1',
+        '-interaction=nonstopmode',
+        '-shell-escape',
+      },
+    }
   end,
 }
