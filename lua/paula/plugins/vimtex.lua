4b9b5956758ba261f5726243fbdf05482d380520
Paula Amaya <dev@paulaamaya.com>
2026-01-20 13:23:48 -0500

    Notes working on mac

--- lua/paula/plugins/vimtex.lua
+++ lua/paula/plugins/vimtex.lua
@@ -2,8 +2,7 @@ return {
   "lervag/vimtex",
   lazy = false,
   config = function()
-    vim.g.vimtex_view_method = "zathura" 
+    vim.g.vimtex_view_method = "skim" 
     vim.g.vimtex_quickfix_mode = 0
-    vim.g.vimtex_view_zathura_use_synctex = 0
   end,
 }
